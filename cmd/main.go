b86b24a94139f3dbc44873e738f0209070c1b5a6
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-07-13 23:32:24 -0300

    view funcional com count

---
+++ cmd/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"io"
+	"text/template"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type Templates struct {
+	templates *template.Template
+}
+
+func (t *Templates) Render(w io.Writer, name string, data any, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func newTemplates() *Templates {
+	return &Templates{
+		templates: template.Must(template.ParseGlob("views/*.html")),
+	}
+}
+
+type Count struct {
+	Count int
+}
+
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+
+	count := Count{Count: 0}
+	e.Renderer = newTemplates()
+
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(200, "index", count)
+	})
+
+	e.POST("/count", func(c echo.Context) error {
+		count.Count++
+		return c.Render(200, "index", count)
+	})
+
+	e.Logger.Fatal(e.Start(":42069"))
+}
