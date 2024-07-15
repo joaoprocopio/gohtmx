34a6dfff6c8d0b3f829fd6fb09b5d7b7fd8a1d1e
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-07-15 00:58:09 -0300

    limpa um pouco

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"io"
+	"net/http"
 	"text/template"
 
 	"github.com/labstack/echo/v4"
@@ -40,6 +41,15 @@ type Data struct {
 	Contacts Contacts
 }
 
+func (d *Data) hasEmail(email string) bool {
+	for _, contact := range d.Contacts {
+		if contact.Email == email {
+			return true
+		}
+	}
+	return false
+}
+
 func newData() Data {
 	return Data{
 		Contacts: []Contact{
@@ -49,25 +59,58 @@ func newData() Data {
 	}
 }
 
+type FormData struct {
+	Values map[string]string
+	Errors map[string]string
+}
+
+func newFormData() FormData {
+	return FormData{
+		Values: make(map[string]string),
+		Errors: make(map[string]string),
+	}
+}
+
+type Page struct {
+	Data Data
+	Form FormData
+}
+
+func newPage() Page {
+	return Page{
+		Data: newData(),
+		Form: newFormData(),
+	}
+}
+
 func main() {
 	e := echo.New()
 
 	e.Use(middleware.Logger())
 
-	data := newData()
+	page := newPage()
 	e.Renderer = newTemplates()
 
 	e.GET("/", func(c echo.Context) error {
-		return c.Render(200, "index", data)
+		return c.Render(http.StatusOK, "index", page)
 	})
 
 	e.POST("/contacts", func(c echo.Context) error {
 		name := c.FormValue("name")
 		email := c.FormValue("email")
 
-		data.Contacts = append(data.Contacts, newContact(name, email))
+		if page.Data.hasEmail(email) {
+			formData := newFormData()
+			formData.Values["name"] = name
+			formData.Values["email"] = email
+			formData.Errors["email"] = "Email already exists"
 
-		return c.Render(200, "contacts", data)
+			return c.Render(http.StatusUnprocessableEntity, "form", formData)
+		}
+
+		page.Data.Contacts = append(page.Data.Contacts, newContact(name, email))
+
+		return c.Render(http.StatusOK, "contacts", page)
 	})
 
 	e.Logger.Fatal(e.Start(":42069"))
